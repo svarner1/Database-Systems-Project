52c8be8b6ac485e442a7ca4508f701cf1cb5fee7
Morgan <morganw2001@gmail.com>
2023-04-10 18:20:17 -0400

    added seller_history, delivery, and category table

--- create.sql
+++ create.sql
@@ -3,3 +3,25 @@ CREATE TABLE payment(
     customerUsername    VARCHAR(25),
     sellerID            in
 )
+
+CREATE TABLE seller_history(
+    sellerID            INTEGER NOT NULL,
+    orderID             INTEGER NOT NULL,
+    revenue             INTEGER NOT NULL,
+    FOREIGN KEY (sellerID) REFERENCES seller(id) ON DELETE CASCADE
+);
+
+CREATE TABLE delivery (
+    deliveryID          SERIAL PRIMARY KEY,
+    customerUsername    VARCHAR(25),
+    orderID             INTEGER NOT NULL,
+    status              
+    sellerID            INTEGER NOT NULL
+
+);
+
+CREATE TABLE category (
+
+
+    
+)
