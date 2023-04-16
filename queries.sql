77d11cf4f984fd2039ffbeb4e7c21b18e4517d31
Candace Ehimen <candaceehimen@gmail.com>
2023-04-16 19:09:48 -0400

    fixed layout

--- queries.sql
+++ queries.sql
@@ -1,21 +1,21 @@
---When the customer enters the website, they can see the list of products along with the image, the price, the rating, and the seller's brandname. (Bullet pt 1)
+-- FQ 1: When the customer enters the website, they can see the list of products along with the image, the price, the rating, and the seller's brandname.
 SELECT p.name, p.image, p.rating, p.price,s.brandname
 FROM product AS p, seller AS s
 WHERE p.sellerID = s.id;
 
--- They will be allowed to filter the view of the products by the highest rating. (Bullet pt 1)
+-- FQ 1: They will be allowed to filter the view of the products by the highest rating.
 SELECT p.name, p.image, p.rating, p.price, s.brandname
 FROM product AS p, seller AS s
 WHERE p.sellerID = s.id
 ORDER BY p.rating DESC;
 
--- They will be allowed to filter the view of the products from low to the highest price (Bullet pt 1)
+-- FQ 1: They will be allowed to filter the view of the products from low to the highest price.
 SELECT p.name, p.image, p.rating, p.price, s.brandname
 FROM product AS p, seller AS s
 WHERE p.sellerID = s.id
 ORDER BY p.price ASC;
 
---They will be allowed to filter the view of the products from a specific seller. (Bullet pt 1)
+-- FQ 1: They will be allowed to filter the view of the products from a specific seller. 
 SELECT p.name, p.image, p.rating, p.price, s.brandname
 FROM product AS p, seller AS s
 WHERE p.sellerID = s.id AND s.brandName ='ElectricVodka';
@@ -37,7 +37,7 @@ FROM product AS p, seller AS s
 WHERE p.sellerID = s.id AND s.brandName ='SeltzersRUS';
 
 
---The customer also has the option to view the products by a particular category. This will show the customer the specific products that are part of the category chosen and the seller's brandname. (Bullet pt 2) 
+-- FQ 2: The customer also has the option to view the products by a particular category. This will show the customer the specific products that are part of the category chosen and the seller's brandname.
 SELECT p.name, p.image, p.rating, p.price, p.category, s.brandname
 FROM product AS p, category AS c, seller AS s
 WHERE c.category_name = 'Rum' AND p.category = c.category_name AND p.sellerID = s.id;
@@ -68,7 +68,7 @@ WHERE c.category_name = 'Hard Seltzers' AND p.category = c.category_name AND p.s
 
 
 
---A customer’s order history will be ordered from most recent to least recent. The details for each order will include specific information related to the order(total cost, order date) and delivery info. (Bullet pt 3)
+-- FQ 3 :A customer’s order history will be ordered from most recent to least recent. The details for each order will include specific information related to the order(total cost, order date) and delivery info.
 SELECT o.id, o.customerusername , o.totalprice, c.size, o.date
 FROM orders as o
 INNER JOIN order_history oh ON o.id = oh.orderid
@@ -104,39 +104,8 @@ INNER JOIN cart c ON o.id=c.id
 WHERE o.customerusername = 'shotsonme'
 ORDER BY o.date DESC;
 
--- Retrieving information about seller (bullet point 9)
-SELECT * FROM seller 
-WHERE id = 1;
 
-SELECT * FROM seller 
-WHERE id = 2;
-
-SELECT * FROM seller 
-WHERE id = 3;
-
-SELECT * FROM seller 
-WHERE id = 4;
-
-SELECT * FROM seller 
-WHERE id = 5;
-
--- Retrieving all products offered by a particular seller (bullet point 9)
-SELECT * FROM product 
-WHERE sellerID = 1;
-
-SELECT * FROM product 
-WHERE sellerID = 2;
-
-SELECT * FROM product 
-WHERE sellerID = 3;
-
-SELECT * FROM product 
-WHERE sellerID = 4;
-
-SELECT * FROM product 
-WHERE sellerID = 5;
-
---When the customer looks at the specific order from the history, they are able to see the products that were in the order.(Bullet pt 4)
+--FQ 4: When the customer looks at the specific order from the history, they are able to see the products that were in the order.
 SELECT o.id, o.customerusername, p.name
 FROM adds_product as a
 INNER JOIN orders o ON a.cartID = o.id
@@ -197,10 +166,7 @@ INNER JOIN orders o ON a.cartID = o.id
 INNER JOIN product p ON a.productID = p.id
 WHERE customerUsername = 'beerpong'AND o.id=10;
 
-
-
--- Retrieving a customer's order history (ordered from most to least recent)
--- product, cost, delivery info needed
+-- FQ 5:Retrieving a customer's order history (ordered from most to least recent) product, cost, delivery info needed
 SELECT  o2.customerUsername, 
         o2.date, 
         o2.totalPrice, 
@@ -217,8 +183,7 @@ WHERE  c.customerUsername = 'solocup'
 GROUP BY o.orderID
 ORDER BY o.orderID DESC;
 
--- Seller History - calculates and stores revenue
-
+-- FQ 6:As part of a seller’s sell history, the total profit a seller makes is calculated and stored as revenue
 SELECT SUM(sh.revenue)
 FROM seller_history as sh
 WHERE sh.sellerID = 1;
@@ -238,6 +203,46 @@ WHERE sh.sellerID = 4;
 SELECT SUM(sh.revenue)
 FROM seller_history as sh
 WHERE sh.sellerID = 5;
+
+
+-- FQ 9: Retrieving information about seller (bullet point 9)
+SELECT * FROM seller 
+WHERE id = 1;
+
+SELECT * FROM seller 
+WHERE id = 2;
+
+SELECT * FROM seller 
+WHERE id = 3;
+
+SELECT * FROM seller 
+WHERE id = 4;
+
+SELECT * FROM seller 
+WHERE id = 5;
+
+-- FQ 9: Retrieving all products offered by a particular seller (bullet point 9)
+SELECT * FROM product 
+WHERE sellerID = 1;
+
+SELECT * FROM product 
+WHERE sellerID = 2;
+
+SELECT * FROM product 
+WHERE sellerID = 3;
+
+SELECT * FROM product 
+WHERE sellerID = 4;
+
+SELECT * FROM product 
+WHERE sellerID = 5;
+
+
+
+
+
+
+
 
 
 
